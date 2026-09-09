b79d677cae286df6128246cda874d0abb1ee2b9a
cerimonialbabr <cerimonial.babr@gmail.com>
2026-09-09 15:13:22 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.SGCES_CONFIG = {
   APP_NAME: 'SGCES',
   APP_FULL_NAME: 'Sistema de Gerenciamento de Cerimonial em Eventos Sociais',
   VERSION: '1.1.0',
-  API_URL: 'COLE_AQUI_A_URL_DO_WEB_APP',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzvZnov2JOGgfZ1pIP1qZa4KXlsr0AGxOGYtH-l0lXfeFlazbtCgjhKG7BGI9LL-a-9qQ/exec',
   POLL_RECEPTION_MS: 8000,
   POLL_TV_MS: 10000
 };
