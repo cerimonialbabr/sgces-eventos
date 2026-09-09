0ed602ed1163cfd46f2dd911aa368a5ddc6eff39
cerimonialbabr <cerimonial.babr@gmail.com>
2026-09-09 14:54:01 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+window.SGCES_CONFIG = {
+  APP_NAME: 'SGCES',
+  APP_FULL_NAME: 'Sistema de Gerenciamento de Cerimonial em Eventos Sociais',
+  VERSION: '1.1.0',
+  API_URL: 'COLE_AQUI_A_URL_DO_WEB_APP',
+  POLL_RECEPTION_MS: 8000,
+  POLL_TV_MS: 10000
+};
