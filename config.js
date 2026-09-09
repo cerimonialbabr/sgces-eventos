65857bef2c72a54089c4ea5b88ad85caab096af4
cerimonialbabr <cerimonial.babr@gmail.com>
2026-09-09 13:28:06 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.SGM_CONFIG = {
+  API_URL: 'COLE_AQUI_A_URL_DO_APPS_SCRIPT_EXEC',
+  APP_NAME: 'SGCM-Mesas',
+  CREDIT_LINE_1: 'Maj Av Ranyer - Seção de Cerimonial Militar da BABR',
+  CREDIT_LINE_2: 'Versão 1.0 - 2026'
+};
