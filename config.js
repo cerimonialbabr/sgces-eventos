668752b572ec2759dcd0350adb2265d7d374a48e
cerimonialbabr <cerimonial.babr@gmail.com>
2026-09-09 14:00:19 -0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-window.SGM_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbzrjDLXAluz5lpuVuuQS0IX7GqB48ykaZgkbDgu-j0zw-_Ua13YWmnJ1Tz_5g3Q8Sd-iw/exec',
-  APP_NAME: 'SGCM-Mesas',
-  CREDIT_LINE_1: 'Maj Av Ranyer - Seção de Cerimonial Militar da BABR',
-  CREDIT_LINE_2: 'Versão 1.0 - 2026'
-};
